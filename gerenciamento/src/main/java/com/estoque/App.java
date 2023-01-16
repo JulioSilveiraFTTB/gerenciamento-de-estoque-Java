025cd1e594257d392ec2bbd5b75f32e942a1ff66
Júlio Silveira <julio.silveira@ayvu.net>
2023-01-16 11:36:01 -0300

    objeto Nota Fiscal (atributos, métodos e teste)

--- gerenciamento/src/main/java/com/estoque/App.java
+++ gerenciamento/src/main/java/com/estoque/App.java
@@ -7,8 +7,12 @@ public class App
     public static void main( String[] args )
     {
         Produto produto;
+        NotaFiscal nf;
         
         produto = new Produto("Teste", "Apenas testando...", 10);
         System.out.println(produto.toString());
+
+        nf = new NotaFiscal(null, null);
+        System.out.println(nf.toString());
     }
 }
--- gerenciamento/src/main/java/com/estoque/objetos/NotaFiscal.java
+++ gerenciamento/src/main/java/com/estoque/objetos/NotaFiscal.java
@@ -1,5 +1,65 @@
 package com.estoque.objetos;
 
-public class NotaFiscal {
-    
+import java.io.Serializable;
+import java.util.Random;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class NotaFiscal implements Serializable {
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    private Integer codigo;
+    private final LocalDate dataEmissao;
+    private Item relacaoItem;
+
+    /**
+     * Construtor do objeto Nota Fiscal
+     * 
+     * @param dataEmissao
+     * @param relacaoItem
+     */
+    public NotaFiscal(LocalDate dataEmissao, Item relacaoItem){
+        Random random = new Random();
+
+        this.codigo = random.nextInt(100000);
+        this.dataEmissao = LocalDate.now();
+        this.relacaoItem = relacaoItem;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public Integer getCodigo(){
+        return codigo;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public LocalDate getDataEmissao(){
+        return dataEmissao;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public Item getRelacaoItem(){
+        return relacaoItem;
+    }
+
+    /**
+     * Retorna a string completa do produto
+     * 
+     * @return
+     */
+    @Override
+    public String toString(){
+        return "Informações da NF: \n" 
+        + "Código: " + codigo + "\n"
+        + "Data de emissão: " + dtf.format(dataEmissao) + "\n"
+        + "Relação de items: " + relacaoItem + "\n";
+    }
 }
