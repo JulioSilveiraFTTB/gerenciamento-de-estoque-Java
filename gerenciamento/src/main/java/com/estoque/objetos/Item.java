97c99744b1c4092b8d5576da8498f6275175498f
Júlio Silveira <julio.silveira@ayvu.net>
2023-01-16 14:29:04 -0300

    objeto Item (atributos, métodos e teste)

--- gerenciamento/src/main/java/com/estoque/objetos/Item.java
+++ gerenciamento/src/main/java/com/estoque/objetos/Item.java
@@ -1,5 +1,17 @@
 package com.estoque.objetos;
 
 public class Item {
-    
+    private Produto produto;
+
+    public Item(Produto produto){
+        this.produto = produto;
+    }
+
+    public Produto getProduto(){
+        return produto;
+    }
+
+    public Produto setProduto(Produto produto){
+        return this.produto = produto;
+    }
 }
